d7d0bbbdddaaffd10aa750bce919cf6d22584b0f
fersubair40 <olajnrsubair@gmail.com>
2018-10-08 12:16:27 +0100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2018_10_06_134249) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stickies", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-end
 
+end
